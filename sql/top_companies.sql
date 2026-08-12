9efc54448ef1f0f60678dabf5c12a9adb5379ac1
premmohan-dev <premsai.a.mohan@gmail.com>
2026-08-12 17:15:55 -0400

    Refactor SQL query for top companies complaints
    Updated SQL query to select top companies by complaint count.

--- sql/top_companies.sql
+++ sql/top_companies.sql
@@ -1,7 +1,10 @@
-SELECT
+-- Research Question 1
+-- Which companies received the most credit card complaints from Virginia consumers?
+-- Purpose: Identifies the companies with the highest number of complaints in the dataset.
+
+SELECT TOP 10
     Company,
     COUNT(*) AS Complaint_Count
-FROM complaints
+FROM ConsumerComplaints
 GROUP BY Company
-ORDER BY Complaint_Count DESC
-LIMIT 10;
+ORDER BY COUNT(*) DESC;
